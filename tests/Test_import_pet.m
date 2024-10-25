4a8b30165484643c2d778628f607f491cb0b6ce0
Diellor Basha <49167439+DiellorBasha@users.noreply.github.com>
2024-10-25 12:42:48 -0400

    Added requirements for the creation of a new PET data type in Brainstorm

--- tests/Test_import_pet.m
+++ tests/Test_import_pet.m
@@ -1,49 +1,69 @@
 classdef Test_import_pet < matlab.unittest.TestCase
     methods(Test)
+        
+        
         function testDefaultInputs(testCase)
-            % Test 1: No inputs provided (except for required ones)
+         
+            % Check if Brainstorm is running
+          if isempty(bst_get ('isGUI'))
+              brainstorm
+          end
+          
+          % Test 1: No inputs provided (except for required ones)
             iSubject = 1;
-            MriFile = '';
-            [BstMriFile, sMri, Messages] = import_pet(iSubject, MriFile);
+            PetFile = '';
+            [BstPetFile, sPet, Messages] = import_pet(iSubject, PetFile);
             
             % Verify that default values are assigned
-            testCase.verifyEqual(BstMriFile, [], 'BstMriFile should be empty');
-            testCase.verifyEqual(sMri, [], 'sMri should be empty');
+            testCase.verifyEqual(BstPetFile, [], 'BstPetFile should be empty');
+            testCase.verifyEqual(sPet, [], 'sPet should be empty');
             testCase.verifyEqual(Messages, [], 'Messages should be empty');
         end
         
         function testPartialInputs(testCase)
+           
+                        % Check if Brainstorm is running
+          if isempty(bst_get ('isGUI'))
+              brainstorm
+          end
+            
             % Test 2: Some inputs provided, others missing
             iSubject = 1;
-            MriFile = '';
+            PetFile = '';
             FileFormat = 'Nifti';
             isInteractive = 1;
             
-            [BstMriFile, sMri, Messages] = import_pet(iSubject, MriFile, FileFormat, isInteractive);
+            [BstPetFile, sPet, Messages] = import_pet(iSubject, PetFile, FileFormat, isInteractive);
             
             % Verify that the provided inputs are used and defaults are applied for missing inputs
-            testCase.verifyEqual(BstMriFile, [], 'BstMriFile should be empty');
-            testCase.verifyEqual(sMri, [], 'sMri should be empty');
+            testCase.verifyEqual(BstPetFile, [], 'BstPetFile should be empty');
+            testCase.verifyEqual(sPet, [], 'sPet should be empty');
             testCase.verifyEqual(Messages, [], 'Messages should be empty');
             testCase.verifyEqual(FileFormat, 'Nifti', 'FileFormat should be Nifti');
             testCase.verifyEqual(isInteractive, 1, 'isInteractive should be 1');
         end
         
         function testAllInputsProvided(testCase)
+           
+                        % Check if Brainstorm is running
+          if isempty(bst_get ('isGUI'))
+              brainstorm
+          end
+            
             % Test 3: All inputs provided
             iSubject = 1;
-            MriFile = '';
+            PetFile = '';
             FileFormat = 'DICOM';
             isInteractive = 1;
             isAutoAdjust = 0;
             Comment = 'TestComment';
             Labels = {'Label1'};
             
-            [BstMriFile, sMri, Messages] = import_pet(iSubject, MriFile, FileFormat, isInteractive, isAutoAdjust, Comment, Labels);
+            [BstPetFile, sPet, Messages] = import_pet(iSubject, PetFile, FileFormat, isInteractive, isAutoAdjust, Comment, Labels);
             
             % Verify that all provided inputs are used
-            testCase.verifyEqual(BstMriFile, [], 'BstMriFile should be empty');
-            testCase.verifyEqual(sMri, [], 'sMri should be empty');
+            testCase.verifyEqual(BstPetFile, [], 'BstPetFile should be empty');
+            testCase.verifyEqual(sPet, [], 'sPet should be empty');
             testCase.verifyEqual(Messages, [], 'Messages should be empty');
             testCase.verifyEqual(FileFormat, 'DICOM', 'FileFormat should be DICOM');
             testCase.verifyEqual(isInteractive, 1, 'isInteractive should be 1');
